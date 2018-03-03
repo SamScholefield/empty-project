3d172ec6d50a3ba2fa9df652486f23cf65ab8b24
Sam Scholefield <sam.scholefield@gmail.com>
2018-03-03 22:18:07 +0100

    GA by router event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        (<any>window).ga("set", "page", event.urlAfterRedirects);
+        (<any>window).ga("send", "pageview");
+      }
+    });
+  }
 }
